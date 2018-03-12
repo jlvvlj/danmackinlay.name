3f6166f2bc1a118ade3dfebf070625d205e94773
Dan MacKinlay <myopenid@email.possumpalace.org>
2018-03-12 18:06:28 +1100

    URLs

--- publish.sh
+++ publish.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 rsync -av --partial --progress --delete --exclude=.git --exclude=".DS_Store" --exclude="*.sh" --exclude="*.py" ./ bodyworker@web570.webfaction.com:webapps/danmackinlay_name/
